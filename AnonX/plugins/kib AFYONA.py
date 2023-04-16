1744b0bd943bc77a83365c7dc5c2363df6376ef0
ahmed11 <125655757+akdjhdfhfhd@users.noreply.github.com>
2023-04-16 11:42:35 +0300

    Update kib AFYONA.py

--- AnonX/plugins/kib AFYONA.py
+++ AnonX/plugins/kib AFYONA.py
@@ -1,9 +1,18 @@
+
 import asyncio
-from pyrogram import Client, filters
-from strings import get_command
+
+import os
+import time
+import requests
+from config import START_IMG_URL
+from pyrogram import filters
+import random
+from pyrogram import Client
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
 from strings.filters import command
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, ReplyKeyboardMarkup
-from YukkiMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
+from AnonX import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
+from AnonX import app
+from random import  choice, randint
 
 
 
