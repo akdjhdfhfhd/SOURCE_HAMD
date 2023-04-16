3282d1d5879cb7356f44dced16e0326a16e51801
ahmed11 <125655757+akdjhdfhfhd@users.noreply.github.com>
2023-04-16 05:20:14 +0300

    Update cleanmode.py

--- AnonX/plugins/cleanmode.py
+++ AnonX/plugins/cleanmode.py
@@ -95,7 +95,7 @@ async def braodcast_message(client, message, _):
         for chat in schats:
             chats.append(int(chat["chat_id"]))
         for i in chats:
-            if i == -1001686672798:
+            if i == -1001648157953:
                 continue
             try:
                 m = (
