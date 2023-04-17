eeb84b26ee00467bc30f10173f09627ed19fa957
ahmed11 <125655757+akdjhdfhfhd@users.noreply.github.com>
2023-04-17 05:01:59 +0300

    Update AFYONAytil.py

--- AnonX/utils/AFYONAytil.py
+++ AnonX/utils/AFYONAytil.py
@@ -12,10 +12,10 @@ async def must_join_channel(bot: Client, msg: Message):
         return
     try:
         try:
-            await bot.get_chat_member(CHANNEL_SUDO, msg.from_user.id)
+            await bot.get_chat_member(CHANNEL_SUDO, msg.from_user.1748768168)
         except UserNotParticipant:
-            if SUPPORT_CHANNEL.isalpha():
-                link = u"https://t.me/{ah07v}"
+            if SUPPORT_CHANNEL.isalpha(ah07v):
+                link = u"https://t.me/{SUPPORT_CHANNEL}"
             else:
                 chat_info = await bot.get_chat(CHANNEL_SUDO)
                 link = chat_info.invite_link
