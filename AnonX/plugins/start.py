0ba38276b2c05675bddb4b1a79b1e61b53996064
ahmed11 <125655757+akdjhdfhfhd@users.noreply.github.com>
2023-04-17 19:57:23 +0300

    Update start.py

--- AnonX/plugins/start.py
+++ AnonX/plugins/start.py
@@ -224,9 +224,11 @@ async def start_comm(client, message: Message, _):
 )
 @LanguageStart
 async def testbot(client, message: Message, _):
-    out = start_pannel(_)
-    return await message.reply_text(
-        _["start_1"].format(
+    OWNER = OWNER_ID[0]
+    out = start_pannel(_, app.username, OWNER)
+    return await message.reply_photo(
+               photo=config.START_IMG_URL,
+               caption=_["start_1"].format(
             message.chat.title, config.MUSIC_BOT_NAME
         ),
         reply_markup=InlineKeyboardMarkup(out),
