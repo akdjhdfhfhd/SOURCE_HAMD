1b16c38563a28a68b88f0dfbca835ad068387e39
ahmed11 <125655757+akdjhdfhfhd@users.noreply.github.com>
2023-04-18 05:58:46 +0300

    Update kibmostml.py

--- AnonX/plugins/kibmostml.py
+++ AnonX/plugins/kibmostml.py
@@ -199,3 +199,25 @@ async def ihd(client: Client, message: Message):
         )
     )
 
+@app.on_message(
+    command(["ايدي","id","/id"])
+    & filters.group
+    & ~filters.edited
+)
+async def iddd(client, message):
+    if message.chat.id in iddof:
+      return
+    usr = await client.get_chat(message.from_user.id)
+    name = usr.first_name
+    photo = await app.download_media(usr.photo.big_file_id)
+    await message.reply_photo(photo,       caption=f"""🤡 ¦𝙽𝙰𝙼𝙴 :{message.from_user.mention}\n🎯 ¦𝚄𝚂𝙴𝚁 :@{message.from_user.username}\n🎃 ¦𝙸𝙳 :`{message.from_user.id}`\n💌 ¦𝙱𝙸𝙾 :{usr.bio}\n✨ ¦𝙲𝙷𝙰𝚃: {message.chat.title}\n♻️ ¦𝙸𝙳.𝙶𝚁𝙾𝚄𝙿 :`{message.chat.id}`""", 
+    reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        name, url=f"https://t.me/{message.from_user.username}")
+                ],
+            ]
+        ),
+    )
+    
