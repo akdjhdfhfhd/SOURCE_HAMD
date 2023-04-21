f40c3c23f3e55e49795598209aca452b2a97206a
ahmed11 <125655757+akdjhdfhfhd@users.noreply.github.com>
2023-04-21 18:27:04 +0300

    Create ادمن.py

---
+++ AnonX/plugins/ادمن.py
@@ -0,0 +1,71 @@
+import asyncio
+import os
+from pyrogram.types import CallbackQuery
+from AnonX import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
+from AnonX import app
+import requests
+import pyrogram
+from pyrogram import Client, emoji 
+from config import *
+from pyrogram import filters
+from strings.filters import command
+from config import OWNER_ID
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
+from pyrogram.errors import MessageNotModified
+
+
+
+@app.on_message(
+    command("admin")
+) 
+async def cr_usage(_, callback_query: CallbackQuery):
+    await callback_query.answer()
+    await callback_query.message.edit_text(
+        text="""**★⊷⧼𝚂𝙾𝚄𝚁𝙲𝙴 𝙷𝙰𝙼𝙳⧽⊶★**
+★¦ اهلا بك عزيزي في قسم اوامر تشغيل الادمن
+★¦ رفع ثانوي
+★¦ تنزيل ثانوي
+★¦ قائمة الثانويين
+★¦ رفع ادمن
+★¦ تنزيل ادمن
+★¦ قائمة الادمن
+★¦ حظر
+★¦ الغاء الحظر
+★¦ المحظورين
+★¦ حظر عام
+★¦ الغاء الحظر العام
+★¦ المحظورين عام
+★¦ اونلاين
+★¦ اذاعه
+★¦ تحديث
+★¦ logger
+★¦ ريلود
+★¦ وقف
+★¦ كمل
+★¦ اسكت
+★¦ اتكلم
+★¦ ايقاف
+★¦ تخطي
+★¦ @all
+★¦ all stop
+★¦ يوتيوب / تنزيل
+★¦ playing
+★¦ القائمه
+★¦ حذف القائمه
+★¦ تحديث
+★¦ الاحصائيات
+★¦ لايف
+★¦ مساعده
+★¦ الاعدادت
+★¦ بينج
+**★⊷⧼𝚂𝙾𝚄𝚁𝙲𝙴 𝙷𝙰𝙼𝙳⧽⊶★**""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "𝚂𝙾𝚄𝚁𝙲𝙴 𝙷𝙰𝙼𝙳", url=f"https://t.me/ah05v"),
+                    
+                ]
+            ]
+        )
+    )
