c442444809ed34b23ee764c2d8b9a2afceab6f3a
ahmed11 <125655757+akdjhdfhfhd@users.noreply.github.com>
2023-04-22 03:25:58 +0300

    Update start.py

--- AnonX/utils/inline/start.py
+++ AnonX/utils/inline/start.py
@@ -19,7 +19,7 @@ def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
                 callback_data="settings_back_helper",
             ),
             InlineKeyboardButton(
-                text="الاعدادات ⚙️", callback_data="settings_back_helper"
+                text="الاعدادات ⚙️", callback_data="settings_helper"
             ),
         ],
      ]
