a559bc536d0c5b0ec915c04479e1bcfcf8707837
ahmed11 <125655757+akdjhdfhfhd@users.noreply.github.com>
2023-04-22 04:27:57 +0300

    Update play.py

--- AnonX/plugins/play.py
+++ AnonX/plugins/play.py
@@ -27,24 +27,6 @@ from AnonX.utils.inline.playlist import botplaylist_markup
 from AnonX.utils.logger import play_logs
 from AnonX.utils.stream.stream import stream
 
-
-force_btn = InlineKeyboardMarkup(
-    [
-        [
-            InlineKeyboardButton(
-              text="•قناة البوت•", url="https://t.me/ah07v",),                        
-        ],        
-    ]
-)
-async def check_is_joined(message):    
-    try:
-        userid = message.from_user.id
-        status = await app.get_chat_member(f"@ah07v", userid)
-        return True
-    except Exception:
-        await message.reply_text("⚠️︙عذراً، عليك الانضمام الى القناة أولاً :",reply_markup=force_btn,parse_mode="markdown",disable_web_page_preview=False)
-        return False
-
 # Command
 PLAY_COMMAND = get_command("PLAY_COMMAND")
 
