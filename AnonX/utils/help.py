29d3d406c5a40ec3f5081568de3f3bac11f49756
ahmed11 <125655757+akdjhdfhfhd@users.noreply.github.com>
2023-04-22 03:17:58 +0300

    Update help.py

--- AnonX/utils/help.py
+++ AnonX/utils/help.py
@@ -12,7 +12,7 @@ def help_pannel(_, START: Union[bool, int] = None):
     second = [
         InlineKeyboardButton(
             text=_["BACK_BUTTON"],
-            callback_data=f"settingsback_helper",
+            callback_data=f"settings_back_helper",
         ),
         InlineKeyboardButton(
             text=_["CLOSEMENU_BUTTON"], callback_data=f"close"
